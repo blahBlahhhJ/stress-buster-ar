79b07960626e441cba421b32fbb3314b3038d8e2
Jason Wang <blahblahj.wsy@gmail.com>
2020-05-01 22:29:37 -0700

    Add really cool welcome page & animation

--- Stress Buster AR/Controller/StartViewController.swift
+++ Stress Buster AR/Controller/StartViewController.swift
@@ -10,13 +10,40 @@ import UIKit
 
 class StartViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var playButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setBackgroundImg()
+        titleLabel.layer.masksToBounds = true
+        titleLabel.layer.cornerRadius = 10
+        titleLabel.layer.borderWidth = 3
+        titleLabel.layer.borderColor = UIColor.black.cgColor
         playButton.layer.cornerRadius = 10
+        playButton.layer.borderWidth = 3
+        playButton.layer.borderColor = playButton.currentTitleColor.cgColor
+        titleFadeIn()
+    }
+    
+    private func titleFadeIn() {
+        titleLabel.center.y += 180
+        UIView.animate(withDuration: 0.5) {
+            self.titleLabel.center.y -= 180
+        }
+    }
+    
+    private func setBackgroundImg() {
+        let width = self.view.bounds.width
+        let height = self.view.bounds.height
+        if width < height {
+            backgroundImageView.image = UIImage(named: "starting_page_portrait")
+        } else {
+            backgroundImageView.image = UIImage(named: "starting_page_landscape")
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
